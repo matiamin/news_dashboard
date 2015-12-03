be57740b9f7c5f4cf01969bf197f312975f691e3
Brendan Mahoney <BMahoney08@gmail.com>
2015-12-03 14:58:47 -0500

    up and running

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var methodOverride = require("method-override");
 //may need to rename this database!!!
 mongoose.connect("mongodb://localhost/users");
 var app = express();
-app.use(Express.statis(path.join(__disname, "/public")));
+// app.use(express.static(__dirname, "/public"));
 app.use(bodyParser.json());
 
 
