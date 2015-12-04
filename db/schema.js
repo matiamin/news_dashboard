e088829ef736ff0374ff85d9a6c12f13b5975ccf
Matiullah Amin <matiullah243@gmail.com>
2015-12-04 15:27:16 -0500

    Adds seed data

--- db/schema.js
+++ db/schema.js
@@ -2,7 +2,23 @@
 var mongoose = require('mongoose');
 // instantiate a name space for our Schema constructor defined by mongoose.
 var Schema = mongoose.Schema,
-    ObjectId = Schema.ObjectId;
+  ObjectId = Schema.ObjectId;
+//will add later
+// var ArticleSchema = new Schema({
+//   title: String,
+//   url: String,
+//   body: String,
+//   source: String,
+//   date: String
+// });
+
+// defining schema for topics
+var TopicSchema = new Schema({
+  keyword: String,
+  //Add this to schema upon twitter api integration
+  // source: String
+  // articles: [ArticleSchema]
+});
 
 // defining schema for users.
 var UserSchema = new Schema({
@@ -11,23 +27,9 @@ var UserSchema = new Schema({
   topics: [TopicSchema]
 });
 
-// defining schema for topics
-var TopicSchema = new Schema({
-  keyword: String,
-  //Add this to schema upon twitter api integration
-  // source: String
-  articles: [ArticleSchema]
-});
-
-var ArticleSchema = new Schema({
-  title: String,
-  url: String,
-  body: String,
-  source: String,
-  date: String
-});
 // setting models in mongoose utilizing schemas defined above, we'll be using
 // these frequently throughout our app
 mongoose.model("User", UserSchema);
 mongoose.model("Topic", TopicSchema);
-mongoose.model("Article", ArticleSchema);
+//add later
+// mongoose.model("Article", ArticleSchema);
