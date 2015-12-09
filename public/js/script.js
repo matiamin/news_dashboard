bc044487f836943ea3d3d6aa1a1f0504dccd93ea
Brendan Mahoney <BMahoney08@gmail.com>
2015-12-09 15:51:41 -0500

    Added API functionality

--- public/js/script.js
+++ public/js/script.js
@@ -1,20 +1,25 @@
 $( document ).ready(function() {
-//creating new article object
-// var article = {
-//   search: function(keyword){
-//     var url = 'https://api.datamarket.azure.com/Bing/Search/v1/Composite?Sources=%27news%27&Query=%27'+ keyword + '%27&$format=json';
-//     $.ajax({
-//       dataType: "json",
-//       url: url,
-//       type: "GET"
-//     }).done(function(response) {
-//       console.log(response);
-//     });
-//   },
-// };
   console.log("ready!");
+  var searchKeyword = function(keyword) {
+    var url = 'https://api.datamarket.azure.com/Bing/Search/v1/Composite?Sources=%27news%27&Query=%27'+ encodeURI(keyword) + '%27&$format=json';
+    $.ajax({
+      dataType: 'json',
+      url: url,
+      headers : {
+        'Authorization': 'Basic Ok1IckdZMENJM3ZFeWdUSFFJZEIxb2xIT3BKNnZ0L3pUU21jeFFKSkxPV2s='
+      },
+      type: 'GET'
+    }).done(function(response) {
+      for(var i = 0; i < 5; i++) {
+        $('.articles').append("<div><h1><a href='" + response.d.results[0].News[i].Url + "'>" + response.d.results[0].News[i].Title + "</a></h1></div>");
+      }
+    }).fail(function() {
+      console.log('failed!');
+    });
+  };
+
   $('.topic').on('click', function() {
-    console.log('clicked!');
-    console.log(this.val());
+    console.log("clicked!");
+    searchKeyword($(this).text());
   });
 });
